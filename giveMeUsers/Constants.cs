49b890764eae32d0c05847a18c3291a8ecada8a4
Stuart May <stumay1992@gmail.com>
2023-02-19 10:46:02 +1100

    update api key after it randomly stopped working

--- giveMeUsers/Constants.cs
+++ giveMeUsers/Constants.cs
@@ -3,6 +3,6 @@
     public static class Constants
     {
         // I didn't want to put a mutable variable in a class but now it's dependant on this class. Should probably use a factory instead
-        public static readonly string GitHubAPIKey = "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";
+        public static readonly string GitHubAPIKey = "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";
     }
 }
